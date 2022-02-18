91db75ef5e5fafab3e64a37e8123454f98050a06
Nazarius Teka <an1teka4@gmail.com>
2022-02-18 22:57:58 +0000

    CMake

---
+++ CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/hello.m.o"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
